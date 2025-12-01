2f783953da6a62d694df3f231e20f10cec59271d
Nicholas Hansen <ndhansen@leskat.net>
2025-12-01 05:31:09 +0000

    Stupid but it worked

--- src/aoc_2025/day_1.py
+++ src/aoc_2025/day_1.py
@@ -3,7 +3,39 @@ from typing import Any
 from aoc.puzzle import PuzzleInput
 
 
-def part_1(puzzle: PuzzleInput) -> Any: ...
+def part_1(puzzle: PuzzleInput) -> Any:
+    total = 0
+    dial = 50
+    for line in puzzle.lines:
+        direction = line[0]
+        count = int(line[1:])
+        if direction == "L":
+            dial -= count
+        if direction == "R":
+            dial += count
+        if dial < 0 or dial > 99:
+            dial %= 100
+        if dial == 0:
+            total += 1
+    return total
 
 
-def part_2(puzzle: PuzzleInput) -> Any: ...
+def part_2(puzzle: PuzzleInput) -> Any:
+    total = 0
+    dial = 50
+    for line in puzzle.lines:
+        direction = line[0]
+        count = int(line[1:])
+        while count > 0:
+            if direction == "L":
+                dial -= 1
+            if direction == "R":
+                dial += 1
+            count -= 1
+
+            if dial < 0 or dial > 99:
+                dial %= 100
+
+            if dial == 0:
+                total += 1
+    return total
